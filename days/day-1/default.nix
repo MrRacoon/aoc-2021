b805589890b3d37ffc9ebf9d48e74971a575505d
Erik Sutherland <erik.misu@gmail.com>
2021-12-01 14:51:51 -0800

    add day 1 part 2

--- days/day-1/default.nix
+++ days/day-1/default.nix
@@ -3,8 +3,11 @@ let
   inherit (builtins) readFile map length;
   inherit (pkgs.lib)
   pipe splitString filter stringLength tail zipListsWith toInt;
+
+  tripleZip = xs: zipListsWith (z: y: z + y) (zipListsWith (x: y: x + y) xs (tail xs)) (tail (tail xs));
 in
-  pipe ./input.txt [
+{
+  "Part1" = pipe ./input.txt [
     readFile
     (splitString "\n")
     (filter (x: stringLength x > 0))
@@ -12,4 +15,15 @@ in
     (xs: zipListsWith (x: y: y - x) xs (tail xs))
     (filter (x: x > 0))
     (length)
-  ]
+  ];
+  "Part2" = pipe ./input.txt [
+    readFile
+    (splitString "\n")
+    (filter (x: stringLength x > 0))
+    (map toInt)
+    tripleZip
+    (xs: zipListsWith (x: y: y - x) xs (tail xs))
+    (filter (x: x > 0))
+    (length)
+  ];
+}
